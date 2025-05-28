de41b40c478b22b2a1d44fc0d9a42136e20719d1
Omid <omid@gmail.com>
2025-05-28 17:03:20 +0330

    'added widget'

--- school/teachers/form.py
+++ school/teachers/form.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class TeacherForm(forms.Form):
-    name = forms.CharField(label='your name', label_suffix='=', max_length=25)
+    name = forms.CharField(label='your name', label_suffix='=', max_length=25, widget=forms.TextInput({'class':'form-control'}))
     email = forms.EmailField(help_text='your email should gmail account', required=True, error_messages={'required': 'please enter your email'})
